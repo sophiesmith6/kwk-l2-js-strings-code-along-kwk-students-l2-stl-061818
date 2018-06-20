fd63130db736b1078199fb706d727f9e1a0f577d
sophiesmith6 <18smiths@corjesu.org>
2018-06-20 20:55:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Write your code in this file!
 
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Klossybook, '+ currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
